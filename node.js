b8533e32e72f690da38809fdb2947f4c485e8a2e
Kiara Bici <kiara.bici16@gmail.com>
2022-10-24 22:21:50 +0200

    Exercise 4

--- node.js
+++ node.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+var user = {
+  //creating the object property
+  first_name: "John",
+  last_name: "Smith",
+  age: "38",
+  department: "Software",
+};
+console.log(user); // printing the object's properties
+
+delete user.last_name; // deleting the second property
+console.log(user);
+
+var size = Object.keys(user).length; // showing the length of the object
+console.log(size);
