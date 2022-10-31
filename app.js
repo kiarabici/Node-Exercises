d79856dbd7d238656eb9ef1236521b3bab88cc7f
Kiara Bici <kiara.bici16@gmail.com>
2022-10-31 16:38:56 +0100

    Exercise 5

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 "strict mode"; // this is the file about ex.5
 const http = require("http");
-const port = 5000;
+const port = 3000;
 const server = http.createServer(function (req, res) {
-  res.write("This is the string I chose to show...");
+  res.write("nacktschnecke");
   res.end();
 });
 server.listen(port, function (error) {
   if (error) {
     console.log("Something went wrong", error);
   } else {
-    console.log("http://localhost:5000/"); //logging to the console the current url
+    console.log("http://localhost:3000/nacktschnecke"); //logging to the console the current url
   }
 });
